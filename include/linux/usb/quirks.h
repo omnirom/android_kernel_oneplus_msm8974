f03e624ef9a926d33c86eaf0e9ed2c77ede92b0a
Zefan Li <lizefan@huawei.com>
2016-10-09 19:20:47 +0800

    fix upstream

--- include/linux/usb/quirks.h
+++ include/linux/usb/quirks.h
@@ -31,12 +31,8 @@
 #define USB_QUIRK_DELAY_INIT		0x00000040
 
 #define USB_QUIRK_OTG_PET		0x00000080
-
 /* device can't handle device_qualifier descriptor requests */
 #define USB_QUIRK_DEVICE_QUALIFIER	0x00000100
-
-/* device generates spurious wakeup, ignore remote wakeup capability */
-#define USB_QUIRK_IGNORE_REMOTE_WAKEUP	0x00000200
 
 /*
  * For high speed and super speed interupt endpoints, the USB 2.0 and
