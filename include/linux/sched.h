5ef224c3c10d0e24e1c086ab6c7064ead078dcc2
Philipp Vogel <philippwolf86.pw@gmail.com>
2017-06-16 09:37:34 +0200

    add CPUQuiet
    Change-Id: I4cb87432f8b3873e21d17afb5d476f2f2129c393

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -142,8 +142,13 @@ extern unsigned long nr_iowait(void);
 extern unsigned long nr_iowait_cpu(int cpu);
 extern unsigned long this_cpu_load(void);
 
+#if defined(CONFIG_MSM_DCVS)
 extern void sched_update_nr_prod(int cpu, unsigned long nr, bool inc);
 extern void sched_get_nr_running_avg(int *avg, int *iowait_avg);
+#endif
+
+extern unsigned long get_avg_nr_running(unsigned int cpu);
+extern unsigned long avg_nr_running(void);
 
 extern void calc_global_load(unsigned long ticks);
 extern void update_cpu_load_nohz(void);
@@ -1372,9 +1377,9 @@ struct task_struct {
 	unsigned long stack_canary;
 #endif
 
-	/* 
+	/*
 	 * pointers to (original) parent process, youngest child, younger sibling,
-	 * older sibling, respectively.  (p->father can be replaced with 
+	 * older sibling, respectively.  (p->father can be replaced with
 	 * p->real_parent->pid)
 	 */
 	struct task_struct __rcu *real_parent; /* real parent process */
